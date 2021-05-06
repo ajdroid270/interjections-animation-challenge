41f508666df93c945b6fac31bd3f8bb2d923f4f0
Ajay Bhargav Alli <milkyway0earth@gmail.com>
2021-05-06 15:35:16 +0530

    config: added configuration file

---
+++ src/config.js
@@ -0,0 +1,7 @@
+const totalTime = 3000;
+const totalHeight = 300;
+const strokeWidth = 6;
+const thumbRadius = strokeWidth;
+const clearence = strokeWidth / 2;
+
+export { totalTime, totalHeight, clearence, strokeWidth, thumbRadius };
